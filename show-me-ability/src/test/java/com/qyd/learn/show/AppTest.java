0a0ddb4a08669cd04877cdaeb6c93fd3de127d77
deadman <1023979260@qq.com>
2019-05-27 16:45:07 +0800

    算法

---
+++ show-me-ability/src/test/java/com/qyd/learn/show/AppTest.java
@@ -0,0 +1,20 @@
+package com.qyd.learn.show;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
