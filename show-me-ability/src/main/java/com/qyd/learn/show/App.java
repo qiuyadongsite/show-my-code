3aaf00b500c5a1bc2702983d66da04352515a668
deadman <1023979260@qq.com>
2019-05-31 18:14:27 +0800

    tomcat

--- show-me-ability/src/main/java/com/qyd/learn/show/App.java
+++ show-me-ability/src/main/java/com/qyd/learn/show/App.java
@@ -1,5 +1,7 @@
 package com.qyd.learn.show;
 
+import java.util.Vector;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        Vector s=new Vector();
+
+
         System.out.println( "Hello World!" );
     }
 }
